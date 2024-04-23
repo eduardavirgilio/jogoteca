be57b38928210c3a569d95b6c34d590ed21450fb
duda <eduarda.virgilio2630@gmail.com>
2024-04-23 08:25:35 -0300

    add: menu principal

---
+++ main.py
@@ -0,0 +1,15 @@
+#### JOGOTECA ####
+# desenvolvido por: eduarda virgilio
+
+print()
+print("----------------------------------------")
+print("| seja bem-vinda(o) a SUPER ★ JOGOTECA |")
+print("| aqui você encontrará jogos incriveis!|")
+print("----------------------------------------")
+print("| escolha um para jogar:               |")
+print("| 1. jogo da velha                     |")
+print("| 2. jogo da forca                     |")
+print("| 3. jogo da tabuada                   |")
+print("| 4. jogo da adivinhação               |")
+print("----------------------------------------")
+print()
