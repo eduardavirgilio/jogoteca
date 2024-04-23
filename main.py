1cbbf50f4eb9bf12c903e1b79512fb0c5d81a2b1
duda <eduarda.virgilio2630@gmail.com>
2024-04-23 08:31:57 -0300

    add: validação por idade

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ print("----------------------------------------")
 print("| seja bem-vinda(o) a SUPER ★ JOGOTECA |")
 print("| aqui você encontrará jogos incriveis!|")
 print("----------------------------------------")
-print("| escolha um para jogar:               |")
-print("| 1. jogo da velha                     |")
-print("| 2. jogo da forca                     |")
-print("| 3. jogo da tabuada                   |")
-print("| 4. jogo da adivinhação               |")
-print("----------------------------------------")
+
 print()
+
+idade = int(input("digite sua idade: "))
+
+if idade >= 18: 
+    print("----------------------------------------")
+    print("| escolha um para jogar:               |")
+    print("| 1. jogo da velha                     |")
+    print("| 2. jogo da forca                     |")
+    print("| 3. jogo da tabuada                   |")
+    print("| 4. jogo da adivinhação               |")
+    print("----------------------------------------")
+    escolha = int(input("digite o numero de acordo com o jogo escolhido: "))
+
+else:
+    print("você não tem permissão para jogar.")
