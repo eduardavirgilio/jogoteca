cdf85a7938b61542fe74860c9897da37f8dbe831
duda <eduarda.virgilio2630@gmail.com>
2024-04-23 10:04:37 -0300

    add: jogo de adivinhação

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #### JOGOTECA ####
 # desenvolvido por: eduarda virgilio
 
+#if "__main__"
+
+from jogo_de_adivinhacao.jogo_mortal import *
+
+
 import os 
 
 os.system("cls")
@@ -16,13 +21,18 @@ idade = int(input("digite sua idade: "))
 
 if idade >= 18: 
     print("----------------------------------------")
-    print("| escolha um para jogar:               |")
-    print("| 1. jogo da velha                     |")
+    print("| ★  escolha um para jogar:            |")
+    print("| 1. jogo da adivinhação               |")
     print("| 2. jogo da forca                     |")
     print("| 3. jogo da tabuada                   |")
-    print("| 4. jogo da adivinhação               |")
+    print("| 4. jogo da velha                     |")
     print("----------------------------------------")
-    escolha = int(input("digite o numero de acordo com o jogo escolhido: "))
 
 else:
     print("você não tem permissão para jogar.")
+
+escolha = int(input("digite o numero de acordo com o jogo escolhido: "))
+
+if escolha == 1 : #jogo da adivinhação
+    os.system("cls")
+    adivinhacao()
