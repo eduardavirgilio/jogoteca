f012fe7313a59c4e368f53b330aef78ca3c7f929
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2024-04-29 14:06:05 -0300

    add: menu e pergunta da jogada

---
+++ pedra_papel_tesoura/jogo_oficial.py
@@ -0,0 +1,12 @@
+import os 
+
+os.system("cls")
+
+print ("seja bem vindo ao jogo pedra, papel e tesoura!")
+print ("você pode escolher entre pedra, papel e tesoura e o computador irá jogar contra você")
+print ("★")
+print ("boa sorte !")
+print ()
+
+escolha = str(input("faça a sua jogada: "))
+
