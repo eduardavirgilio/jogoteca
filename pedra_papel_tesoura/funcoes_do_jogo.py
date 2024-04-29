6f88a6101f60628cd91d259aa1efcb85763a8cc1
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2024-04-29 15:02:24 -0300

    jogo completo

--- pedra_papel_tesoura/funcoes_do_jogo.py
+++ pedra_papel_tesoura/funcoes_do_jogo.py
@@ -4,9 +4,7 @@ def escolhe_gesto (gesto:str) :
 
     if gesto == "papel" or gesto == "pedra" or gesto == "tesoura" :
 
-        arquivo = open("pedra_papel_tesoura/escolha_aleatoria.txt", "r", encoding='utf8')
-        lista_palavras = arquivo.readlines()
-        arquivo.close()
+        lista_palavras = ["pedra", "papel", "tesoura"]
 
         palavra_escolhida = random.choice(lista_palavras)
         palavra_escolhida_limpa = palavra_escolhida.strip()
--- pedra_papel_tesoura/jogo_oficial.py
+++ pedra_papel_tesoura/jogo_oficial.py
@@ -15,4 +15,36 @@ print ()
 escolha = str(input("faça a sua jogada: "))
 
 gesto_aleatorio = escolhe_gesto (escolha)
-print (gesto_aleatorio)
+print (f"a jogada do computador foi: {gesto_aleatorio}")
+
+#se os dois forem iguais
+if escolha == gesto_aleatorio :
+    print("deu empate")
+
+print()
+#eu ganhando
+
+#voce ganha (papel ganha de pedra)
+if escolha == "papel" and gesto_aleatorio == "pedra":
+    print("você ganhou!")
+
+#voce ganha (pedra ganha de tesoura)
+if escolha == "pedra" and gesto_aleatorio == "tesoura" :
+    print("você ganhou!")
+
+#voce ganha (tesoura ganha de papel)
+if escolha == "tesoura" and gesto_aleatorio == "papel" :
+    print("você ganhou!")
+
+#o pc ganhando
+    
+if gesto_aleatorio == "papel" and escolha == "pedra":
+    print("você perdeu")
+
+#voce ganha (pedra ganha de tesoura)
+if gesto_aleatorio == "pedra" and escolha == "tesoura" :
+    print("você perdeu")
+
+#voce ganha (tesoura ganha de papel)
+if gesto_aleatorio == "tesoura" and escolha == "papel" :
+    print("você perdeu")
